46e730947f91a80b322df5083364db014423e6f3
Ravi vadukul <ravivadukul8@gmail.com>
2025-11-12 23:03:25 +0530

    06:dashboard sideBar

---
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -0,0 +1,73 @@
+import { GeneratedAvatar } from "@/components/gernerated-avatar";
+import { Avatar } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { authClient } from "@/lib/auth-client";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const DashboardUserButton = () => {
+  const { data, isPending } = authClient.useSession();
+  const router = useRouter();
+  const onLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/sign-in");
+        },
+      },
+    });
+  };
+  if (isPending || !data?.user) return <p>Loading...</p>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex flex-items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+        {data.user.image ? (
+          <Avatar>
+            <AvatarImage src={data?.user?.image} />
+          </Avatar>
+        ) : (
+          <GeneratedAvatar
+            seed={data.user.name}
+            variant="botttsNeutral"
+            className="size-9 mr-3"
+          />
+        )}
+        <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+          <p className="text-sm truncate w-full">{data.user.name}</p>
+          <p className="text-xs truncate w-full">{data.user.email}</p>
+        </div>
+        <ChevronDownIcon className="size-4 shrink-0" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" side="right" className="w-72">
+        <DropdownMenuLabel>
+          <div className="flex flex-col gap-1">
+            <span className="font-medium truncate">{data.user.name}</span>
+            <span className="text-sm font-normal text-muted-foreground truncate">
+              {data.user.email}
+            </span>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
+          Billing <CreditCardIcon className="size-4" />
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="cursor-pointer flex items-center justify-between"
+          onClick={onLogout}
+        >
+          Logout <LogOutIcon className="size-4" />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default DashboardUserButton;
