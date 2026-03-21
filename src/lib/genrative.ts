df57e8cd8600ac3cee9851c3b8a7f9f3f2ff5537
Ravi vadukul <ravivadukul8@gmail.com>
2026-03-21 17:07:20 +0530

    23 Connecting Agents (Stream, OpenAI)

---
+++ src/lib/genrative.ts
@@ -0,0 +1,14 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+
+async function generateAIResponse(prompt: string) {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+
+  return response.text();
+}
+
+export default generateAIResponse;
